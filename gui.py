ac312e528af539fbb38ac58eb925425b781d426d
Abdullah Ehsan <abdullahehsan275@gmail.com>
2025-07-01 19:34:39 -0600

    Added code comments

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import streamlit as st
 from rag import rag
 import html
 
-tabs = st.tabs(["Model", "Logs"])
+tabs = st.tabs(["Model", "Logs"]) # two tabs, one for model and one for logs
 
 with tabs[0]:
     st.title("Retrieval Augmented Generation Model")
@@ -19,16 +19,16 @@ with tabs[1]:
     if st.button("Clear logs"):
         with open("logs.log", "w") as file:
             pass
-        st.rerun()
+        st.rerun() # refreshes page
     info = st.empty()
-    logs = open("logs.log", "r")
-    log_history = html.escape(logs.read()).replace("\n", "<br>")
+    reader = open("logs.log", "r") # rereads logs each time the page is opened
+    log_history = html.escape(reader.read()).replace("\n", "<br>") # allows logs to be safely used in html
     html_text = f"""
     <p style="font-family: 'Courier New', monospace; font-size: 14px; color: white;">
         {log_history}
     </p>
-    """
+    """ # html for logs
     st.markdown(html_text, unsafe_allow_html=True)
-    logs.close()
+    reader.close()
 
 
